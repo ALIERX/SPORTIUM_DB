a9c0877c1a5190b07cae5298e8a5b8a75ec5abb7
Alex Khamraev <a.khamraev@gmail.com>
2025-10-20 18:02:35 +0200

    Delete scr directory

--- scr/components/HexagonPattern.tsx
+++
@@ -1,5 +0,0 @@
-export function HexagonPattern({ className = "" }: { className?: string }) {
-  return (
-    <div className={`absolute inset-0 hexagon-pattern opacity-30 pointer-events-none ${className}`} />
-  );
-}
